237c483b42ad0b824708f29d32c4b08f96fe1cce
tourman <8002615@gmail.com>
2025-04-04 22:16:00 +0100

    janny: eslint
    janny: eslint
    
    janny: eslint: caught errors (to squash)

--- packages/janny/eslint.config.mjs
+++ packages/janny/eslint.config.mjs
@@ -1,11 +1,38 @@
+import { includeIgnoreFile } from '@eslint/compat';
 import pluginJs from '@eslint/js';
 import globals from 'globals';
+import { resolve } from 'node:path';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import tseslint from 'typescript-eslint';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
+  includeIgnoreFile(resolve(__dirname, '../../.gitignore')),
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-namespace': 0,
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          fixStyle: 'inline-type-imports',
+        },
+      ],
+      '@typescript-eslint/no-shadow': 2,
+      '@typescript-eslint/no-empty-object-type': 0,
+      '@typescript-eslint/no-unused-vars': [
+        2,
+        {
+          caughtErrors: 'none',
+        },
+      ],
+      'no-console': 'warn',
+    },
+  },
 ];
